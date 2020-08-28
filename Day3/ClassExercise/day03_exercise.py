3a2315acd33f886c14b07b3a7bf9c89b914e92a6
Jin <60522038+dahjinkim@users.noreply.github.com>
2020-08-28 13:33:15 -0500

    Update day03_exercise.py

--- Day3/ClassExercise/day03_exercise.py
+++ Day3/ClassExercise/day03_exercise.py
@@ -2,5 +2,9 @@
 ## Raise a TypeError with an informative message if 'word' is passed as an integer
 ## When done, run the test file in the terminal and see your results.
 def count_vowels(word):
-
-
+    word_list = [i for i in word]
+    vowels = ['a', 'i', 'o', 'u', 'e', 'A', 'I', 'O', 'U', 'E']
+    vowelCount = 0
+    for i in vowels:
+        vowelCount += word_list.count(i)
+    return vowelCount
