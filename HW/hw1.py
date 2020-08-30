762d8a658472021cd6a0acfe868f47ba5d1fa64d
Jin <60522038+dahjinkim@users.noreply.github.com>
2020-08-30 18:50:19 -0500

    Create hw1.py
    started hw1. built basic structure.

---
+++ HW/hw1.py
@@ -0,0 +1,30 @@
+class Portfolio():
+    def __init__(self, cash, stock, mutualfund):
+        self.cash = cash
+        self.stock = stock
+        self.mutualfund = mutualfund
+
+    def addCash(): # adds cash
+
+    def withdrawCash(): # removes cash
+
+    def buyStock(a, b): # buy a share of stock b
+
+    def sellStock(symbol, a): # sell a share of stock with symbol
+
+    def buyMutualFund(a, b): # buy a share of mutualfund b
+
+    def sellMutualFund(symbol, a): # buy a share of mutualfund with symbol
+
+    def history(): # prints list of all transactions ordered by time
+
+
+class Stock():
+    def __init__(self, price, symbol):
+        self.price = price
+        self.symbol = symbol
+
+class MutualFund():
+    def __init__(self, price, symbol):
+        self.price = price
+        self.symbol = symbol
