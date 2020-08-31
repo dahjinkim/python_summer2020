ac173179aac0257d959424bea95a454b5c6a4c15
Jin <60522038+dahjinkim@users.noreply.github.com>
2020-08-31 15:36:03 -0500

    Update hw1.py

--- HW/hw1.py
+++ HW/hw1.py
@@ -1,26 +1,41 @@
 class Portfolio():
-    def __init__(self, cash, stock, mutualfund):
+    def __init__(self, cash = 0, stock = [], mutualfund = []):
         self.cash = cash
         self.stock = stock
         self.mutualfund = mutualfund
 
-    def __str__():
-        return 'cash: {}\nstock: {}\nmutual fund: {}'.format(self.cash, self.stock, self.mutualfund)
+    def __str__(self):
+        return 'cash: ${:.2f}\nstock: {}\nmutual fund: {}'.format(self.cash, self.stock, self.mutualfund)
 
-    def addCash(): # adds cash
+    def addCash(self, money): # adds cash
+        self.cash += money
+        return 'Current cash: {:.2f}'.format(self.cash)
 
-    def withdrawCash(): # removes cash
+    def withdrawCash(self, money): # removes cash
+        self.cash -= money
+        return 'Current cash: {:.2f}'.format(self.cash)
 
-    def buyStock(a, b): # buy a share of stock b
+    def buyStock(self, share, stock):
+        if type(share) != int:
+            return print('Stock must be whole unit')
+        elif self.cash - (share * stock.price) < 0:
+            return print('Not enough money!')
+        else:
+            self.cash -= (share * stock.price)
+            self.stock.append((share, stock.symbol))
 
-    def sellStock(symbol, a): # sell a share of stock with symbol
+    def buyMutualFund(self, share, fund):
+        if self.cash - share < 0:
+            return print('Not enough money!')
+        else:
+            self.cash -= share
+            self.mutualfund.append((share, fund.symbol))
 
-    def buyMutualFund(a, b): # buy a share of mutualfund b
+    # def sellStock(symbol, a): # sell a share of stock with symbol
 
-    def sellMutualFund(symbol, a): # buy a share of mutualfund with symbol
+    # def sellMutualFund(symbol, a): # buy a share of mutualfund with symbol
 
-    def history(): # prints list of all transactions ordered by time
-
+    # def history(): # prints list of all transactions ordered by time
 
 class Stock():
     def __init__(self, price, symbol):
@@ -28,6 +43,17 @@ class Stock():
         self.symbol = symbol
 
 class MutualFund():
-    def __init__(self, price, symbol):
-        self.price = price
+    def __init__(self, symbol):
         self.symbol = symbol
+
+
+portfolio = Portfolio()
+portfolio.addCash(300.50)
+portfolio.withdrawCash(50)
+s = Stock(20, "HFH")
+portfolio.buyStock(5, s)
+portfolio.buyStock(10.3, s)
+portfolio.buyStock(100, s)
+mf1 = MutualFund("BRT")
+portfolio.buyMutualFund(10.3, mf1)
+print(portfolio)
