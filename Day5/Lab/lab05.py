e1832736f60493ef1687030453a407b87e303c47
Jin <60522038+dahjinkim@users.noreply.github.com>
2020-09-04 10:16:02 -0500

    Update lab05.py

--- Day5/Lab/lab05.py
+++ Day5/Lab/lab05.py
@@ -9,22 +9,35 @@ with open("obama-nh.txt", "r") as f:
 ## (although you ~might~ think you could do something like
 ## [l for l in obama if "the" not in l]
 
+index = []
+keyword = re.compile(r"the")
+for i, line in enumerate(obama):
+	if keyword.search(line):
+		print(i)
+		print(line)
+		index.append(i)
 
+no_the = []
+for i, line in enumerate(obama):
+	if i not in index:
+		print(line)
+		no_the.append(line)
+
+len(no_the)
 
 
 # TODO: print lines that contain a word of any length starting with s and ending with e
 
-
+keyword = re.compile(r"^s[a-z]*e$")
+for i, line in enumerate(obama):
+	word = re.split(r'\s', line)
+	for w in word:
+		if keyword.search(w):
+			print(line)
 
 
 ## TODO: Print the date input in the following format
 ## Month: MM
 ## Day: DD
 ## Year: YY
-date = raw_input("Please enter a date in the format MM.DD.YY: ")
-
-
-
-
-
-
+date = input("Please enter a date in the format MM.DD.YY: ")
